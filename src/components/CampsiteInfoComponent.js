8ccb9acfaf8e7103208ee879b2e2ba8450bdc1b9
Reena Kumari <reenanishi1994@gmail.com>
2021-06-24 00:25:33 -0700

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -39,21 +39,18 @@ class CampsiteInfo extends Component {
         return <div />;
     }
 
-
-
-
-
-
     render() {
         if (this.props.campsite) {
-            return <div className="row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-            </div>
-        } else {
-            return <div></div>
-
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            );
         }
+        return <div />;
     }
 }
 
