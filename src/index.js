c74646db78fe1b87a634c62ed6b1c7f3cdd7562c
Reena Kumari <reenanishi1994@gmail.com>
2021-07-12 21:33:09 -0700

    final commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 import 'font-awesome/css/font-awesome.css';
@@ -14,6 +14,7 @@ ReactDOM.render( <React.StrictMode >
     </React.StrictMode>,
     document.getElementById('root')
 );
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
